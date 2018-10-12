464064e06eeee12367288bbccc67c83a3b76b5df
Sid Zhang <haozhigg@gmail.com>
2018-10-12 23:01:22 +0800

    add test.py

---
+++ src/insertion_sort.cc
@@ -0,0 +1,58 @@
+
+#include "../include/rand.h"
+#include <iostream>
+
+
+template<typename T>
+void selection_sort(vector<T>& v) {
+    for (unsigned i = 0; i < v.size() - 1; i++) {
+        unsigned min_index{ i };
+        for (unsigned j = i + 1; j < v.size(); j++)
+            if (v[j] < v[min_index])
+                min_index = j;
+        T tmp = v[i];
+        v[i] = v[min_index];
+        v[min_index] = tmp;
+    }
+}
+
+template<typename T>
+void insertion_sort(vector<T>& v) {
+    for (unsigned i = 1; i < v.size(); i++) {
+        T tmp{ v[i] };
+        int j{ static_cast<int>(i - 1) };
+        while (j >= 0 && v[j] > tmp) {
+            v[j + 1] = v[j];
+            j -= 1;
+        }
+        v[j + 1] = tmp;
+    }
+}
+
+int main(int argc, char** argv) {
+    RandInt rand_int{ -(1 << 20), 1 << 20 };
+    for (unsigned i = 0; i < 10; i++) {
+        vector<int> v0{ rand_int.NextVector(100) };
+        vector<int> v1{ v0 };
+
+        selection_sort(v0);
+        insertion_sort(v1);
+
+        cout << "########################" << endl;
+        cout << "####     Test" << i + 1 << endl;
+        for (const auto& x: v1)
+            cout << x << " ";
+        cout << endl;
+
+        if (v0 == v1) {
+            cout << "####      Pass      ####" << endl;
+            cout << "########################" << endl;
+        }
+        else {
+            cout << "####      Fail      ####" << endl;
+            cout << "########################" << endl;
+        }
+    }
+
+    return 0;
+}
---
+++ test.py
@@ -0,0 +1,63 @@
+
+import sys
+import os
+
+def get_case():
+    assert len(sys.argv) > 1, "Need an extra argument to specify case"
+
+    case = sys.argv[1]
+    cases = [x[:-3] for x in os.listdir("src") if x.endswith(".cc")]
+    match = [x for x in cases if x.find(case) >= 0]
+
+    if len(match) == 0:
+        print("case '{}' not found".format(case))
+        return None
+    elif len(match) > 1:
+        print("multi cases found matching '{}'".format(case))
+        for m in match:
+            print("    {}".format(m))
+        return None
+    else:
+        return match[0]
+
+def compile(case):
+    if not os.path.exists("build"):
+        os.mkdir("build")
+    assert os.path.isdir("build")
+    os.chdir("build")
+
+    cc_file = os.path.join("../src", "{}.cc".format(case))
+    bin_file = os.path.join("{}.exe".format(case))
+
+    ## check modified time of source file and bin file
+    if os.path.getmtime(bin_file) >= os.path.getmtime(cc_file):
+        print("build/{} is up-to-data, no need to rebuild".format(bin_file))
+    else:
+        CXX = r"C:\Program Files\LLVM\bin\clang++.exe"
+        cmd = r"{} -std=c++14 {} -o {}".format(CXX, cc_file, bin_file)
+        os.system(cmd)
+
+    os.chdir("..")
+
+def run(case):
+    if not os.path.exists("test"):
+        os.mkdir("test")
+    assert os.path.isdir("test")
+    os.chdir("test")
+
+    bin_file = os.path.join("../build/{}.exe".format(case))
+    os.system(bin_file)
+
+    os.chdir("..")
+
+def main():
+    case = get_case()
+    if case == None:
+        return
+
+    compile(case)
+    run(case)
+
+if __name__ == "__main__":
+    assert sys.version_info.major >= 3, "This script only runs in Python3"
+    main()
